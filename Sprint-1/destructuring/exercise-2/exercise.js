7ab7a67638287ec6c0dc2e5cb10fd98a346e0000
ike-agu <ikennaww@yahoo.fr>
2025-07-29 18:15:38 +0100

    Implemented hogwartsSchoolTeachers function to display names of teachers who have pets

--- Sprint-1/destructuring/exercise-2/exercise.js
+++ Sprint-1/destructuring/exercise-2/exercise.js
@@ -75,7 +75,6 @@ let hogwarts = [
 ## Task 1
 - In `exercise.js` write a program that will take the `hogwarts` array as input and display the names of the people who belong to the Gryffindor house.
 - Use object destructuring to extract the values you need out of each element in the array.*/
-
 function hogwartsSchool(hogwarts){
   const grinffindors = []
   for(const person of hogwarts){
@@ -87,4 +86,20 @@ function hogwartsSchool(hogwarts){
   return grinffindors;
 }
 
-// console.log(hogwartsSchool(hogwarts))
+
+
+// ## Task 2
+// - In `exercise.js` write a program that will take the `hogwarts` array as input and display the names of teachers who have pets.
+// - Use object destructuring to extract the values you need out of each element in the array.
+function hogwartsSchoolTeachers(hogwarts){
+  const schoolTeachers = []
+  for(const teacher of hogwarts){
+    const {firstName, lastName, pet} = teacher;
+    if( pet !== null ){
+      schoolTeachers.push(`${firstName} ${lastName}`);
+    }
+  }
+  return schoolTeachers;
+}
+
+console.log(hogwartsSchoolTeachers(hogwarts))
